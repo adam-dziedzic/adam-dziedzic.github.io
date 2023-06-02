113c42e86a871473d7fbaface5615bd56ec982ed
Adam Dziedzic <adam.cajf@gmail.com>
2023-06-02 15:19:02 -0400

    deploy at timestamp: 2023-06-02-15:18:16

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 timestamp=$(date "+%Y-%m-%d-%H:%M:%S")
-jekyll build --destination docs
+bundle exec jekyll build --destination docs
 git add *
 git commit -am "deploy at timestamp: ${timestamp}"
 git push origin source
